ea7cfce60004c444fc407c816eb0132512f87730
Oleg <oleg_764@mail.ru>
2022-04-18 13:17:02 +0500

    Zadacha1.2

---
+++ Example008/Program.cs
@@ -0,0 +1,21 @@
+﻿
+  int a,b,c,max ;
+{
+  Console.WriteLine("Введите три числа : ");
+ 
+  a = Convert.ToInt32(Console.ReadLine());
+ 
+  b = Convert.ToInt32(Console.ReadLine());
+
+  c = Convert.ToInt32(Console.ReadLine());
+}
+{max = a;
+ if ( max<b ) 
+  max=b;
+}
+{if (max<c )
+  max=c;
+}
+Console.WriteLine(" MAX= {0} ", max);
+
+Console.ReadLine();
