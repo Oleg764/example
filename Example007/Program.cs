473f02b0cb0542d9c0fc4d604db2be6e6114b1e4
Oleg <oleg_764@mail.ru>
2022-04-18 10:32:46 +0500

    Zadacha1.1

---
+++ Example007/Program.cs
@@ -0,0 +1,29 @@
+﻿  {
+ 
+        int a, b;
+ 
+        Console.WriteLine("Введите два числа : ");
+ 
+        a = Convert.ToInt32(Console.ReadLine());
+ 
+        b = Convert.ToInt32(Console.ReadLine());
+ 
+        if (a > b)
+ 
+        {
+ 
+            Console.WriteLine(" MAX= {0} ", a);
+ 
+        }
+ 
+        else
+ 
+        {
+ 
+            Console.WriteLine(" MAX= {0} ", b);
+ 
+        }
+ 
+        Console.ReadLine();
+ 
+    }
