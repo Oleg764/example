ce01fcc0fbc3415339cf324406d2785543293ed2
Oleg <oleg_764@mail.ru>
2022-04-24 22:28:44 +0500

    Ex.17_D.Z.3.21

---
+++ Example018_D.Z.3.23/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write("Введите число :");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 0)
+    N = -N;
+int i = 1;
+while (i <= N)
+{
+    int result = cub(i);
+
+    Console.Write(result + ";");
+    i++;
+}
+int cub(int N)
+{
+    N = N * N * N;
+    return N;
+}   
