7c9c1b717c099aacd45fd3c3f9250a91b1b45926
Oleg <oleg_764@mail.ru>
2022-04-24 23:26:18 +0500

    Ex.D.Z.3.19

--- Example019_D.Z.3.19/Program.cs
+++ Example019_D.Z.3.19/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//23432 -> да
+//12821 -> да
+Console.Write("Введите пятизначное число:   ");
+string number = Console.ReadLine();
+int length = number.Length;
+
+bool Palindrome()
+{
+    for (int i = 0; i < length / 2; i++)
+        while (number[i] == number[length - 1])
+        {
+            return true;
+        }
+    return false;
+}
+if (Palindrome())
+    Console.Write("Число является полиндромом");
+else
+    Console.Write("Число не является полиндромом");
+Console.WriteLine();
