11937c226a0255be24c6bd5793e1d1ce384db9cc
Oleg <oleg_764@mail.ru>
2022-04-27 21:35:52 +0500

    Ex21.D.Z.4.29

---
+++ Example021_D.Z.4.29/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 случайных целых чисел и выводит их на экран. Диапазон - любое целое число.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+int N = 8;
+
+int[] Array8(int N)
+{
+    
+    int[] arr = new int[N];
+    for (int i = 0; i<N; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
+}
+
+void writeArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+}
+
+writeArray(Array8(N));
