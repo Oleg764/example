98f9d9a891a10d47fe692616968602faa7c4b30e
Oleg <oleg_764@mail.ru>
2022-05-19 11:11:16 +0500

    D.z.7.52

---
+++ Example030_D.Z.7.52/Program.cs
@@ -0,0 +1,69 @@
+﻿//Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int[,] GetMatrix(int m, int n, int min, int max)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(min, max);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j]}  ");
+            
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] ArithmeticMean(int[,] array)
+{
+    double[] result = new double[array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+            result[j] = sum / array.GetLength(1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[] array)
+{
+    Console.WriteLine("------------------------");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+}
+
+int m = 7;
+int n = 5;
+int min = 1;
+int max = 100;
+
+int[,] matrix = GetMatrix(m, n, min, max);
+PrintMatrix(matrix);
+double[] arithmeticMean = ArithmeticMean(matrix);
+PrintArray(arithmeticMean);
+System.Console.WriteLine();
+System.Console.WriteLine();
